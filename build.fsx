b909a7dc11246d366a3ab75745a0034a36397cfa
jbrestan <jan.brestan@live.com>
2017-02-21 22:18:24 +0100

    Use XPlot to render the graph, add initial params

--- build.fsx
+++ build.fsx
@@ -1,8 +1,14 @@
 #I "packages/FAKE/tools/"
+#I "packages/Google.DataTable.Net.Wrapper/lib"
+#I "packages/XPlot.GoogleCharts/lib/net45"
 
 #r "FakeLib.dll"
+#r "Google.DataTable.Net.Wrapper.dll"
+#r "XPlot.GoogleCharts.dll"
 
+open System
 open Fake
+open XPlot.GoogleCharts
 
 let repo =
     let repo = getBuildParam "repo"
@@ -10,19 +16,43 @@ let repo =
         failwith """Please specify repository path as 'repo="<path to repository>"'"""
     else repo
 
+let fileLimit =
+    let limitString = getBuildParamOrDefault "limit" "20"
+    let success, limit = Int32.TryParse(limitString)
+    if (not success) || limit < 0 then
+        failwith "File limit must be a positive integer, or 0 if you want to show all files"
+    else limit
+
 let git commandFormat = Printf.kprintf (fun command -> Git.CommandHelper.getGitResult repo command) commandFormat
 
 Target "ShowStat" (fun _ ->
     let getCommits () = git "rev-list HEAD"
     let getFiles commit = git "diff-tree --no-commit-id --name-only -r %s" commit
-    let getFiles2 commit = git "diff-tree --no-commit-id --name-only -r %s %i" commit 5
+
+    let limitFiles xs =
+        if fileLimit = 0 then xs else Seq.truncate fileLimit xs
+
+    let buildChart (data: seq<string * int>) =
+        let opts =
+            Options(
+                title = "Commits per file",
+                vAxis = Axis(title = "Commit count", minValue = 0),
+                hAxis = Axis(title = "File name", showTextEvery = 1, slantedText = true))
+
+        data
+        |> Seq.map (fun kv -> kv, "Commit count")
+        |> List.ofSeq
+        |> List.unzip
+        |> fun (data, labels) ->
+            Chart.Column(data, labels, opts)
 
     getCommits ()
     |> Seq.collect getFiles
     |> Seq.countBy id
     |> Seq.sortByDescending snd
-    //|> Seq.take 10
-    |> Seq.iter (fun (f,c) -> printfn "\t%i\t%s"c f)
+    |> limitFiles
+    |> buildChart
+    |> Chart.Show
 )
 
 RunTargetOrDefault "ShowStat"
